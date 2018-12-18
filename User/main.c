32e648ac859edb8560e1308157394848d05579e2
Gyrych <gyrych@qq.com>
2018-12-18 15:22:51 +0000

    add loop exit help.

--- User/main.c
+++ User/main.c
@@ -15,9 +15,24 @@ void init_GPIO(void)
 	pinMode(7, OUTPUT);
 }
 
+void water_lamp(void)
+{
+	int i =0;
+
+	for(i = 0; i < 8; i++)
+	{
+		digitalWrite(i, HIGH);
+		delay(100);
+	}
+	for(i = 0; i < 8; i++)
+	{
+		digitalWrite(i, LOW);
+		delay(100);
+	}
+}
+
 int main()
 {
-	int i = 0;
 	char inputstr[1000] = {0};
 
 	init_GPIO();
@@ -28,16 +43,19 @@ int main()
 		scanf("%s" , inputstr);
 		if(strcmp(inputstr, "start") == 0)
 		{
-			for(i = 0; i < 8; i++)
-			{
-				digitalWrite(i, HIGH);
-				delay(100);
-			}
-			for(i = 0; i < 8; i++)
-			{
-				digitalWrite(i, LOW);
-				delay(100);
-			}
+			water_lamp();
+		}
+		else if(strcmp(inputstr, "loop") == 0)
+		{
+			while(1) water_lamp();
+		}
+		else if(strcmp(inputstr, "exit") == 0)
+		{
+			return 0;
+		}
+		else if(strcmp(inputstr, "help") == 0)
+		{
+			printf("start, run once.\nloop, keep running.\nexit, exit operation.\nhelp, get help.\n");
 		}
 		else
 		{
